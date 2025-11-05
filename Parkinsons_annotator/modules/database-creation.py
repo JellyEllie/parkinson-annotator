93b79f2775b449c6ba79f09587c34586764c2e21
OEM Configuration (temporary user) <eleanor.m.pratt@gmail.com>
2025-11-05 12:44:38 +0000

    changing file extention https://github.com/JellyEllie/parkinson-annotator/issues/1

---
+++ Parkinsons_annotator/modules/database-creation.py
@@ -0,0 +1,83 @@
+""" this script is to create the database and the headings for the tables to be populated with the patient data """
+
+# Datatypes for the database explaination: PRIMARY KEY is identified in case wit is necessary to link this table to another later.
+# Data type TEXT allows for characters of any length. Data tpye INTEGER is a whole number of any length. NOT NULL means that it has to be filled when that row is filled
+# Data type CHAR(1) means that all data must be 1 character inlength. The bottom 3 lines will be generated from ClinVar, so need to be able to stare empty
+# AUTOINCREMENT is an easy way to make a column where every number is unique, which is going to feed into how part of the search functions will work.
+
+# import sqlite and os to usue those modules to create the database
+import sqlite3
+import os
+
+# creating a function so that the code only runs when required and not every time
+def generatetable():
+
+    # creates the database. this function aso opens the database if it already exists, but in the particular place, it creates it
+    conn = sqlite3.connect("parkinsons_data.db")
+
+    # creates a cursor object to be able to execute SQL commands
+    cur = conn.cursor()
+
+    # creating variants table to input data into. The first word is the name of the column, the second is the data type. datatype explaid above.
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS variants (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,   
+                name TEXT NOT NULL,                     
+                chromosome INTEGER NOT NULL,            
+                pos INTEGER NOT NULL,                   
+                ref CHAR(1) NOT NULL,                   
+                alt CHAR(1) NOT NULL,
+                hgvs TEXT,                              
+                classification TEXT,
+                gene TEXT
+                )
+            """)
+
+    # save the changes made aka you commit to those changes
+    conn.commit()
+
+
+    # close the connectionto the database
+    conn.close()
+
+# creating a variable for the database to call in the next section
+db_path = "parkinsons_data.db"
+
+# checks to see if the database exists, to create it if it doesn't
+if os.path.exists(db_path):
+    pass
+else:
+    generatetable()
+
+# the below code is all to generate test data to use for the search testing. Can be deleted when input module is running
+
+# opens link to the database, the same as seen in the function
+conn = sqlite3.connect("parkinsons_data.db")
+
+# creates  a cursor object to be able to execute SQL commnds
+cur = conn.cursor()
+# adding testing data
+patients = (
+    ("patientA", 4, 89822305, "C", "G", "pretendhgvs1", "pathogenic", "fakegene1"),
+    ("patientA", 17, 45983420, "G", "T", "pretenthgvs2", "benign", "fakegene2"),
+    ("patient", 17, 2748693, "C", "A", "pretendhgvs3", "VUS", "fakegene3")
+)
+
+# to prevent the testing data to be input everytime this code is run. Change "stop" to "go" to fill in data
+testing = "go"
+if testing == "go":
+    cur.executemany(
+     "INSERT INTO variants (name, chromosome, pos, ref, alt, hgvs, classification, gene) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+     patients)
+
+# save the changes made
+conn.commit()
+
+# testing table
+cur.execute("SELECT * FROM variants")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+# close the connection to the database
+conn.close()
