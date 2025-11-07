a894d48be27dd47c92041ceab501cc40190fe91e
Caitlin <caitlin.blundell@synnovis.co.uk>
2025-11-07 13:08:02 +0000

    re-adding docstrings and comments https://github.com/JellyEllie/parkinson-annotator/issues/1

--- Parkinsons_annotator/modules/database-creation.py
+++ Parkinsons_annotator/modules/database-creation.py
@@ -1,6 +1,15 @@
 """
-Creates and populates the Parkinson’s Annotator database.
+this script is to create the database and the headings for the tables to be populated with the patient data
+Data types:
+    TEXT: Allows for characters of any length.
+    INTEGER: A whole number of any length.
+    CHAR(1): Single-character field.
+Other database parameters:
+    NOT NULL: Field must contain a value.
+    AUTOINCREMENT: Automatically generates unique IDs for each record.
+    PRIMARY KEY: Used to uniquely identify each record and potentially link tables.
 """
+
 
 import sqlite3
 from pathlib import Path
@@ -8,10 +17,13 @@ from pathlib import Path
 def generatetable():
     """Create the database and tables if they don't already exist."""
     db_path = Path(__file__).resolve().parents[1] / "parkinsons_data.db"
+    # creates the database. this function is also used to open the database if it already exists
     conn = sqlite3.connect(db_path)
+    # creates a cursor object to be able to execute SQL commands
     cur = conn.cursor()
 
-    # Create tables
+    # Creating variants table
+    # The first word is the name of the column, the second is the data type. Datatype explained above.
     cur.execute("""
         CREATE TABLE IF NOT EXISTS patients (
             name TEXT NOT NULL PRIMARY KEY
@@ -38,20 +50,24 @@ def generatetable():
         )
     """)
 
+    # Save (i.e. commit) changes
     conn.commit()
     conn.close()
 
-
+# When up and running, won't be needed as wil be called in a different script
 # Main execution
 db_path = Path(__file__).resolve().parents[1] / "parkinsons_data.db"
 
 if not db_path.exists():
     generatetable()
 
-# Add testing data
+
+# opens link to the database, the same as seen in the function
 conn = sqlite3.connect(db_path)
+# creates  a cursor object to be able to execute SQL commnds
 cur = conn.cursor()
 
+# Add testing data
 patient_data = [("patient1",), ("patient2",), ("patient3",)]
 variant_data = [
     (15, 38920, "A", "T", "nm_022089.4:c.1544C>T"),
@@ -59,6 +75,8 @@ variant_data = [
 ]
 combined = [("patient1", 1), ("patient2", 1), ("patient3", 2)]
 
+
+# to prevent the testing data to be input everytime this code is run. Change "stop" to "go" to fill in data
 testing = "go"
 if testing == "go":
     cur.executemany(
@@ -70,7 +88,7 @@ if testing == "go":
         "INSERT INTO patient_variant (patient_name, variant_id) VALUES (?, ?)",
         combined
     )
-
+# save the changes made
 conn.commit()
 
 # Verify
@@ -82,4 +100,5 @@ cur.execute("""
 for row in cur.fetchall():
     print(row)
 
+# close the connection to the database
 conn.close()
