bc1c1106f797c6cbca6a59e6ed00d9079330c3f7
abdinaima <abdi.naima@outlook.com>
2025-11-07 10:35:54 +0000

    Added further tests

--- tests/test_variantvalidator_fetch.py
+++ tests/test_variantvalidator_fetch.py
@@ -11,4 +11,11 @@ def test_fetch_variant_validator_success():
     "OMIM ID": "157140"
     }
     assert result == expected
+
+# Empty variant description provided
+def test_fetch_variant_validator_empty():
+    variant_description = ""
+    with pytest.raises (VariantDescriptionError):
+        test_fetch_variant_validator_empty(variant_description)
+
     
