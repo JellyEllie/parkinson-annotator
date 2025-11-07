ff4e7d5ddbd098305a4c1cb4b625d59f1baeae08
abdinaima <abdi.naima@outlook.com>
2025-11-07 10:26:19 +0000

    Added unit test for variant validator fetch

---
+++ tests/test_variantvalidator_fetch.py
@@ -0,0 +1,14 @@
+import pytest
+from Parkinsons_annotator.utils.variantvalidator_fetch import fetch_variant_validator
+
+# Expected successful API call
+def test_fetch_variant_validator_success():
+    variant_description = "17:45983420:G:T"
+    result = fetch_variant_validator(variant_description)
+    expected = {
+    "HGVS nomenclature": "NM_001377265.1:c.841G>T",
+    "HGNC ID": "HGNC:6893",
+    "OMIM ID": "157140"
+    }
+    assert result == expected
+    
