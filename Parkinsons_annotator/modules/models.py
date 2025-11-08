4fc4641fbfb21f632053439c3f74c5edf72714ef
Caitlin <caitlin.blundell@synnovis.co.uk>
2025-11-08 17:39:16 +0000

    added pep257-compliant docstrings https://github.com/JellyEllie/parkinson-annotator/issues/8

--- Parkinsons_annotator/modules/models.py
+++ Parkinsons_annotator/modules/models.py
@@ -1,6 +1,13 @@
 """
-models.py
-Defines ORM models for the Parkinson’s Annotator database.
+ORM models for the Parkinson’s Annotator database.
+
+This module defines the ORM classes used by SQLalchemy to represent patients and variants in the database
+and the many-to-many relationships between them.
+
+CLasses:
+    Variant: Maps to variant table which contains variant characteristics e.g. HGVS notation
+    Patient: Maps to patient table containing patient names
+    Connector: Maps to linking table between patient and variant table
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
@@ -9,9 +16,8 @@ from sqlalchemy.orm import declarative_base
 # Create base class for declarative mapping
 Base = declarative_base()
 
-
 class Variant(Base):
-    """ORM mapping for the 'variants' table."""
+    """Represent a genetic variant in the 'variants' table."""
     __tablename__ = 'variants'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -25,6 +31,7 @@ class Variant(Base):
     clinvar_id = Column(String)
 
     def __repr__(self):
+        """Return string representation of variant object"""
         return (
             f"<Variant(id={self.id}, chr={self.chromosome}, pos={self.pos}, "
             f"ref={self.ref}, alt={self.alt}, gene={self.gene_symbol})>"
@@ -32,14 +39,20 @@ class Variant(Base):
 
 
 class Patient(Base):
-    """ORM mapping for the 'patients' table."""
+    """Represent a patient in the 'patients' table."""
     __tablename__ = 'patients'
 
     name = Column(String, primary_key=True)
 
 
 class Connector(Base):
-    """ORM mapping for linking patients to variants."""
+    """
+    Represent a link between patients and variants.
+
+    This class maps the association table 'patient_variant'
+    supporting the many-to-many relationship between patients
+    and genetic variants.
+    """
     __tablename__ = 'patient_variant'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
