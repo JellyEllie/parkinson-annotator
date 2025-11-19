53351509e646c633d8481fab3759b281ed8677a4
JellyEllie <eleanor.m.pratt@gmail.com>
2025-11-19 20:51:03 +0000

    updates to database_creation.py so that correct tables are created with updated datatypes to store data better. Test data input removed. See https://github.com/JellyEllie/parkinson-annotator/issues/26

--- Parkinsons_annotator/modules/database_creation.py
+++ Parkinsons_annotator/modules/database_creation.py
@@ -32,14 +32,17 @@ def generatetable():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS variants (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            chromosome INTEGER NOT NULL,
-            pos INTEGER NOT NULL,
-            ref CHAR(1) NOT NULL,
-            alt CHAR(1) NOT NULL,
-            hgvs TEXT,
-            classification TEXT,
-            gene_symbol TEXT,
-            clinvar_id TEXT
+            vcf_form VARCHAR(30),
+            hgvs VARCHAR(50),
+            clinvar_id INTEGER,
+            gene_symbol VARCHAR(10),
+            classification VARCHAR(50),
+            cdna_change VARCHAR(20),
+            clinvar_accession VARCHAR(20),
+            num_records INTEGER,
+            review_status VARCHAR(50),
+            associated_condition VARCHAR(50),
+            clinvar_url VARCHAR(500)
         )
     """)
     cur.execute("""
@@ -50,55 +53,14 @@ def generatetable():
         )
     """)
 
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS genes (
+                    gene_symbol VARCHAR(10) PRIMARY KEY,
+                    gene_link VARCHAR(500)
+                )
+                """)
     # Save (i.e. commit) changes
     conn.commit()
     conn.close()
 
-# When up and running, won't be needed as wil be called in a different script
-# Main execution
-db_path = Path(__file__).resolve().parents[1] / "parkinsons_data.db"
 
-if not db_path.exists():
-    generatetable()
-
-
-# opens link to the database, the same as seen in the function
-conn = sqlite3.connect(db_path)
-# creates  a cursor object to be able to execute SQL commnds
-cur = conn.cursor()
-
-# Add testing data
-patient_data = [("patient1",), ("patient2",), ("patient3",)]
-variant_data = [
-    (15, 38920, "A", "T", "nm_022089.4:c.1544C>T"),
-    (7, 12345, "G", "C", "nm_123456.7:c.876G>C")
-]
-combined = [("patient1", 1), ("patient2", 1), ("patient3", 2)]
-
-
-# to prevent the testing data to be input everytime this code is run. Change "stop" to "go" to fill in data
-testing = "go"
-if testing == "go":
-    cur.executemany(
-        "INSERT INTO variants (chromosome, pos, ref, alt, hgvs) VALUES (?, ?, ?, ?, ?)",
-        variant_data
-    )
-    cur.executemany("INSERT INTO patients (name) VALUES (?)", patient_data)
-    cur.executemany(
-        "INSERT INTO patient_variant (patient_name, variant_id) VALUES (?, ?)",
-        combined
-    )
-# save the changes made
-conn.commit()
-
-# Verify
-cur.execute("""
-    SELECT * FROM patients
-    JOIN patient_variant ON patients.name = patient_variant.patient_name
-    JOIN variants ON patient_variant.variant_id = variants.id
-""")
-for row in cur.fetchall():
-    print(row)
-
-# close the connection to the database
-conn.close()
